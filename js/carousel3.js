0cb4e2dda90510028df4b3a8ea7888cd63357dce
mspragi <msprksh45@gmail.com>
2021-03-25 20:22:08 +0530

    my commit

---
+++ js/carousel3.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $('#autoWidth4,#autoWidth5').lightSlider({
+        autoWidth:true,
+        loop_continue:true,
+        onSliderLoad: function() {
+            $('#autoWidth4,#autoWidth5').removeClass('cS-hidden');
+        } 
+    });  
+  });
+
+  
